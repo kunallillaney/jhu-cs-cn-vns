f47e8643108fa05e4e80649a66f3317255357525
raghuram.sv@gmail.com <raghuram.sv@gmail.com@2021630c-cb94-e523-f739-a303f900f283>
2012-04-28 04:10:17 +0000

    added

--- src/stub_sr/sr_router.c
+++ src/stub_sr/sr_router.c
@@ -76,6 +76,62 @@ void z_printICMPpacket(uint8_t* packet,int len)
 
 }/* end of z_printICMPpacket*/
 
+/*---------------------------------------------------------------------
+ * Method: void z_printICMPtimepacket(uint8_t * packet, unsigned int len)
+ *
+ * Print an ICMP packet
+ *---------------------------------------------------------------------*/
+void z_printICMPtimepacket(uint8_t* packet,int len)
+{
+	printf("======== ICMP Time Exceeded packet ========\n");
+	
+	/*
+	* 8 bit: Type
+	* 8 bit: Code
+	* 8 bit: Checksum
+	* 32 bit: Unused
+	* 160 bit: Internet Header
+	* 64 bit: Orginal Data in Datagram
+	*/
+	//8.bit: ICMP type
+	printf("ICMP Type: ");
+	printf("%02X ", packet[20]);
+	printf("\n");
+	
+	//8.bit: ICMP code
+	printf("ICMP Code: ");
+	printf("%02X ", packet[21]);
+	printf("\n");
+	
+	//16.bit: ICMP checksum
+	printf("ICMP checksum: ");
+	for (int i = 22; i < 24; i++) {
+		printf("%02X ", packet[i]);
+	}
+	printf("\n");
+	
+	//16.bit: ICMP unused
+	printf("ICMP unused: ");
+	for (int i = 24; i < 28; i++) {
+		printf("%02X ", packet[i]);
+	}
+	printf("\n");
+	
+	//160.bit: IP header
+	printf("IP header: ");
+	for (int i = 28; i < 48; i++) {
+		printf("%02X ", packet[i]);
+	}
+	printf("\n");
+	
+	//64.bit: Data from Datagram
+	printf("Datagram Data: ");
+	for (int i = 48; i < 54; i++) {
+		printf("%02X ", packet[i]);
+	}
+	printf("\n");
+
+}/* end of z_printICMPtimepacket*/
 
 time_t getCurrentTimeInSeconds() {
 	time_t current;
